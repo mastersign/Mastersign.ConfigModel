aefcdc19b2c9af876f5a96cdcb8c071a92e6437e
Tobias Kiertscher <tobias.kiertscher@th-brandenburg.de>
2023-05-02 11:38:44 +0000

    added tests for type resolution for properties in indirectly associated classes

---
+++ Mastersign.ConfigModel.Test/IndirectTypeDiscriminationByIndicatorPropertyTest.cs
@@ -0,0 +1,50 @@
+﻿using static Mastersign.ConfigModel.Test.TestData;
+
+namespace Mastersign.ConfigModel.Test
+{
+    [TestClass]
+    public class IndirectTypeDiscriminationByIndicatorPropertyTest
+    {
+        private static readonly string SCENARIO = "TypeDiscrimination";
+
+
+        [TestMethod]
+        public void IndirectTypeDiscriminationByExistenceTest()
+        {
+            var mgr = new ConfigModelManager<RootModel>();
+            var modelFile = GetTestDataFilePath(SCENARIO, "IndirectByPropertyExistence.yaml");
+            mgr.AddLayer(modelFile);
+            var root = mgr.LoadModel();
+            var result = root.Base as ModelA;
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.Child, typeof(ChildModelA));
+        }
+
+        class RootModel
+        {
+            public BaseModel? Base { get; set; }
+        }
+
+        abstract class BaseModel
+        {
+        }
+
+        class ModelA : BaseModel
+        {
+            [TypeIndicator]
+            public string? A { get; set; }
+
+            public ChildBaseModel? Child { get; set; }
+        }
+
+        abstract class ChildBaseModel
+        {
+        }
+
+        class ChildModelA : ChildBaseModel
+        {
+            [TypeIndicator]
+            public string? ChildA { get; set; }
+        }
+    }
+}
---
+++ Mastersign.ConfigModel.Test/IndirectTypeDiscriminationByPropertyValueTest.cs
@@ -0,0 +1,45 @@
+﻿using static Mastersign.ConfigModel.Test.TestData;
+
+namespace Mastersign.ConfigModel.Test
+{
+    [TestClass]
+    public class IndirectTypeDiscriminationByPropertyValueTest
+    {
+        private static readonly string SCENARIO = "TypeDiscrimination";
+
+
+        [TestMethod]
+        public void IndirectTypeDiscriminationByValueTest()
+        {
+            var mgr = new ConfigModelManager<BaseModel>();
+            var modelFile = GetTestDataFilePath(SCENARIO, "IndirectByPropertyValue.yaml");
+            mgr.AddLayer(modelFile);
+            var result = mgr.LoadModel() as ModelA;
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.Child, typeof(ChildModelA));
+        }
+
+        class BaseModel
+        {
+            [TypeDiscriminator]
+            public string? Type { get; set; }
+        }
+
+        [TypeDiscriminationValue("Class A")]
+        class ModelA : BaseModel
+        {
+            public ChildBaseModel? Child { get; set; }
+        }
+
+        abstract class ChildBaseModel
+        {
+            [TypeDiscriminator]
+            public string? ChildType { get; set; }
+        }
+
+        [TypeDiscriminationValue("Child Class A")]
+        class ChildModelA : ChildBaseModel
+        {
+        }
+    }
+}
